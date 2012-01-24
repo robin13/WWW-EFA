8a4a4c2eb7ef4654f969f1705761d06fd8fb5872
Robin Clarke <robin@robinclarke.net>
2012-01-24 09:11:00 +0100

    Completed PREREQ_PM in Makefile.PL

--- lib/WWW/EFA.pm
+++ lib/WWW/EFA.pm
@@ -39,11 +39,11 @@ WWW::EFA - Interface to EFA sites (Elektronische Fahrplanauskunft)
 
 =head1 VERSION
 
-    Version 0.03
+    Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
